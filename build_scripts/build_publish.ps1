55c5b1bdc822cdf72f5a551037667328109918c8
Leandro Amodey <leandroamodey@gmail.com>
2025-10-14 17:35:58 -0300

    Build scripts

--- build_scripts/build_publish.ps1
+++ build_scripts/build_publish.ps1
@@ -1,1 +1,12 @@
+# Build Windows con el runtime incluido
 dotnet publish -c Release -r win-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false --self-contained
+
+# Build MacOS con runtime incluido para CPUs Intelx86
+dotnet publish -c Release -r osx-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false --self-contained
+
+# Build MacOS para Apple Silicon
+dotnet publish -c Release -r osx-arm64 -p:PublishReadyToRun=false -p:TieredCompilation=false --self-contained
+
+
+# Build para Linux x64
+dotnet publish -c Release -r linux-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false --self-contained
